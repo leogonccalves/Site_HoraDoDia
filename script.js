e5cf711f5f15034b7765e6bd7c47fee842a3964f
Leonardo Gonçalves <Contato.leonardog@hotmail.com>
2022-01-17 16:16:13 -0300

    arquivos site

---
+++ script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes ()
+    msg.innerHTML=`Agora são: ${hora}hrs e ${min}min`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'imagem/FotoManha.png'
+        document.body.style.background = '#8c7a56'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'imagem/FotoTarde.png'
+        document.body.style.background = '#bc500b'
+    } else {
+        //BOA NOITE!
+        img.src = 'imagem/FotoNoite.png'
+        document.body.style.background = '#1a2d2e'
+    }
+
+}
