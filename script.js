bd4c1e9dd89cd25d120ec90612991b7a80118080
Leonardo Gonçalves <Contato.leonardog@hotmail.com>
2022-01-17 18:30:49 -0300

    arquivos

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    var hora =  20 //data.getHours()
     var min = data.getMinutes ()
     msg.innerHTML=`Agora são: ${hora}hrs e ${min}min`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
-        img.src = 'imagem/FotoManha.png'
-        document.body.style.background = '#8c7a56'
+        img.src = 'imagem/FotoManha.png' 
+        document.body.style.background = '#faedba'
     } else if (hora >= 12 && hora <= 18) {
         //BOA TARDE!
         img.src = 'imagem/FotoTarde.png'
-        document.body.style.background = '#bc500b'
+        document.body.style.background = '#ffce89'
     } else {
         //BOA NOITE!
         img.src = 'imagem/FotoNoite.png'
