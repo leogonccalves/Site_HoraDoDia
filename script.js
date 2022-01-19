13ce4b5005782090f54f116e5465bf81c4a558bb
Leonardo Gonçalves <Contato.leonardog@hotmail.com>
2022-01-19 19:13:14 -0300

    Alterações no código

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora =  20 //data.getHours()
+    var hora = data.getHours()
     var min = data.getMinutes ()
     msg.innerHTML=`Agora são: ${hora}hrs e ${min}min`
     if (hora >= 0 && hora < 12) {
